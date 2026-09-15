6c9d5531e5d423453ea0f6d010686c6d9cbfb844
Robin Jonsson <robinjonsson94@gmail.com>
2026-09-15 22:38:29 +0200

    feat: adderat tre subklasser till Annons

---
+++ DigitalResurs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReserveraAnnons
+{
+    public class DigitalResurs : Annons
+    {
+        public string Filformat { get; private set; }
+        public string Leveranssatt { get; private set; }
+
+        public DigitalResurs(Student saljare, Kurs kurs, string titel, decimal pris, AnnonsSkick skick, DateTime publiceringsdatum, string filformat, string leveranssatt)
+            : base(saljare, kurs, titel, pris, skick, publiceringsdatum)
+        {
+            Filformat = filformat;
+            Leveranssatt = leveranssatt;
+        }
+    }
+}
