e5beea1e77c85f579af2a97c308f4ecdacad3931
Jessica <jessehomman@gmail.com>
2026-09-16 16:16:38 +0200

    feat: add Kurs class & uppdaterat student class

---
+++ Kurs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReserveraAnnons
+{
+    public class Kurs
+    {
+        public string Kurskod { get; private set;  }
+        public string Namn { get; private set; }
+
+        public Kurs(string kurskod, string namn)
+        {
+            Kurskod = kurskod;
+            Namn = namn;
+        }
+    }
+}
--- Student.cs
+++ Student.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace ReserveraAnnons
 {
-    internal class Student
+    public class Student
     {
+        public string Fornamn { get; private set; }
+        public string Efternamn { get; private set; }
+        public string Epostadress { get; private set; }
+        public string Telefonnummer { get; private set; }
+
+        public Student(
+            string fornamn,
+            string efternamn,
+            string epostadress,
+            string telefonnummer)
+        {
+            Fornamn = fornamn;
+            Efternamn = efternamn;
+            Epostadress = epostadress;
+            Telefonnummer = telefonnummer;
+        }
     }
 }
