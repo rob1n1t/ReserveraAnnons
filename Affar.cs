3c102954605abed54a938168f4778ac8c1e1e46b
Jessica <jessehomman@gmail.com>
2026-09-16 17:10:06 +0200

    add AffarsStatus.cs & Affar.cs

---
+++ Affar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReserveraAnnons
+{
+    public class Affar
+    {
+        public Student Kopare { get; private set; }
+        public Annons Annons { get; private set; }
+        public DateTime Reservationsdatum { get; private set; }
+        public AffarsStatus Status { get; private set; }
+
+        public Affar(Student kopare, Annons annons, DateTime datum)
+        {
+            Kopare = kopare;
+            Annons = annons;
+            Reservationsdatum = datum;
+            Status = AffarsStatus.Reserverad;
+        }
+    }
+}
